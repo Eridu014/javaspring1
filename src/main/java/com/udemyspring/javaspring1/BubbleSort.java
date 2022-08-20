f0d85ffee5d3abf370c5239ceaa53ac0de4b3f9b
Eridu014 <chadjames3307@gmail.com>
2022-08-20 17:07:30 -0400

    Add primary component to Bubblesort

--- src/main/java/com/udemyspring/javaspring1/BubbleSort.java
+++ src/main/java/com/udemyspring/javaspring1/BubbleSort.java
@@ -2,8 +2,10 @@ package com.udemyspring.javaspring1;
 
 
 import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Primary;
 
 @Component
+@Primary
 public class BubbleSort implements SortAlgo {
     public int[] sort(int[] numbers) {
         //logic for bubble sort
